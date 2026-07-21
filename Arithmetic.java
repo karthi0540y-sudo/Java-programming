86703e4bda0a0833eb9bc10c0a97d49a83eacf11
karthi0540y-sudo <kavikarthi0540y@gmail.com>
2026-07-21 16:04:40 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        a = 10;
+        b = 20;
+        c = a + b;
+
+        System.out.println(c);
+        System.out.println(a + b);
+
+        a = c + 10;
+        System.out.println(a);
+    }
+}
