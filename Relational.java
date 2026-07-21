ee7307b1bbb2a633d0bbe90a721d5aa415a2a333
karthi0540y-sudo <kavikarthi0540y@gmail.com>
2026-07-21 16:19:16 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,14 @@
+public class Relational {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 10;
+        b = 20;
+
+        System.out.println(a > b);
+        System.out.println(10 < 20);
+        System.out.println(a < b);
+        // System.out.println(10 = 20); // Invalid in Java
+        System.out.println(a == b);
+    }
+}
